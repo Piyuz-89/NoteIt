9f0a61d93ab55438cb92c5307031965e3861d3f5
Piyuz89 <piyushzambre08@gmail.com>
2023-10-10 01:14:26 +0530

    fixed mongoose error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser : true});
+// mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser : true});
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true})
+    await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser : true, useUnifiedTopology: true})
             .then(()=> console.log("MongoDb connected to server"))
             .catch(err => console.log(err));
 }
